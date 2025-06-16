612c55e7af951f01424b8579bc09d6d8b9bc376b
hom9ki <156588482+hom9ki97@users.noreply.github.com>
2025-06-16 20:24:24 +0300

    Формирование вывода информации и транзакциях и балнсе на страницу

--- finance/models.py
+++ finance/models.py
@@ -1,13 +1,38 @@
 from django.db import models
 
 class Transaction(models.Model):
-    amount = models.CharField()
-    income = models.BooleanField()
-    category = models.CharField()
-    created_at = models.DateTimeField()
+    TRANSACTION_TYPE = (('income','Доход'),
+                        ('outcome','Расход'))
+    name = models.CharField(max_length=100, verbose_name='Название', blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма')
+    transaction_type = models.CharField(choices=TRANSACTION_TYPE, verbose_name='Тип операции')
+    category = models.ForeignKey('Category',max_length=50, on_delete=models.PROTECT,null=True, blank=True,
+                                 verbose_name='Категория')
+    date = models.DateField(verbose_name='Дата операции')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+        verbose_name = "Транзакция"
+        verbose_name_plural = "Транзакции"
+        ordering = ['-date']
 
     def __str__(self):
-        return f'{self.amount} {self.income}'
+        return f'{self.name} {self.created_at} '
 
 class Category(models.Model):
-    title = models.CharField()
+    name = models.CharField(max_length=50, verbose_name='Название')
+    color = models.CharField(
+        max_length=20, default='primary', verbose_name='Цвет(Bootstrap)',
+        help_text='primary, secondary, success, danger, warning, info, light, dark'
+    )
+    icon = models.CharField(
+        max_length=50, default='bi-tag', verbose_name='Иконка (Bootstrap Icons)',
+        help_text='Название иконки из https://icons.getbootstrap.com/'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
--- finance/views.py
+++ finance/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from unicodedata import category
+from django.db.models import Sum
+
+from .models import Transaction, Category
+
 
 def index(request):
-    return render(request, 'finance.html')
+    pos_amount = Transaction.objects.filter(transaction_type='income').aggregate(Sum('amount'))['amount__sum'] or 0
+    neg_amount = Transaction.objects.filter(transaction_type='outcome').aggregate(Sum('amount'))['amount__sum'] or 0
+    total = pos_amount - neg_amount
+    categories = Category.objects.all()
+    transactions = Transaction.objects.all()
+    print(pos_amount, neg_amount, categories)
+    context = {
+        'pos_amount': pos_amount,
+        'neg_amount': neg_amount,
+        'total': total,
+        'categories': categories,
+        'transactions': transactions
+
+    }
+    return render(request, 'finance.html', context)
